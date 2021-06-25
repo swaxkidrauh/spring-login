6aeefa61bf6130e39a601f0a0ebe7fd613944eab
swaxkidrauh <swaxkidrauh@users.noreply.github.com>
2021-06-25 11:42:58 +0200

    first configurations

---
+++ src/main/java/com/example/demo/UserHappinessApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserHappinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHappinessApplication.class, args);
+	}
+
+}
